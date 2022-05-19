a31e736bfca43e8bf61563b743732667091d733d
Jonathan Luu <jonathanluu17@gmail.com>
2022-05-18 19:55:23 -0700

    started create tables as select method, need to test

--- ksqljs/ksqlJS.js
+++ ksqljs/ksqlJS.js
@@ -82,6 +82,15 @@ class ksqljs {
       .catch(error => console.log(error));
     }
 
+  //---------------------Create tables as select-----------------
+  createTableAs = (name, columnsArr, topic, value_format = 'json', partitions=1, fromName) => {
+    const selectColStr = columnsArr.reduce((result, current) => result + ', ' + current);
+    const query = `CREATE TABLE ${name} WITH (kafka_topic='${topic}', value_format='${value_format}', partitions=${partitions}) AS SELECT ${selectColStr} FROM ${fromName} EMIT CHANGES;`
+
+    return axios.post(this.ksqldbURL + '/ksql', { ksql: query })
+    .then(res => res)
+    .catch(error => console.log(error));
+  }
   //---------------------Insert Rows Into Existing Streams-----------------
   insertStream = (stream, rows) => {
     return new Promise((resolve, reject) => {
