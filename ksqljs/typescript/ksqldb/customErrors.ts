dc9ee669d86c55cc3259f0499a9bebd63d85a156
Gerry Bong <ggbong734@gmail.com>
2022-07-08 18:40:33 -0700

    added typescript files from gb/typescript4 branch, added gitignore to root directory to ignore node_modules

---
+++ ksqljs/typescript/ksqldb/customErrors.ts
@@ -0,0 +1,70 @@
+/** This file contains custom Node.js errors that extends the built-in Error class
+ * REF: https://rclayton.silvrback.com/custom-errors-in-node-js
+ * REF: https://futurestud.io/tutorials/node-js-create-your-custom-error
+ */
+
+// used to wrap error received from ksqlDB server
+class ksqlDBError extends Error {
+  constructor(error: Error) {
+    super(error.message);
+
+    // Ensure the name of this error is the same as the class name
+    this.name = this.constructor.name;
+
+    // capturing the stack trace keeps the reference to your error class
+    Error.captureStackTrace(this, this.constructor);
+
+    // you may also assign additional properties to your error
+    //this.status = 404
+    Object.keys(error).forEach((property) => {
+      //@ts-ignore
+      this[property] = error[property];
+    });
+  }
+}
+
+// for returning error related to use of queryBuilder class
+class QueryBuilderError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class EmptyQueryError extends QueryBuilderError {
+  constructor() {
+    super("Query should not be empty, undefined, or null");
+  }
+}
+
+class NumParamsError extends QueryBuilderError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+class InappropriateStringParamError extends QueryBuilderError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+class invalidArgumentTypes extends Error {
+  constructor(message: string) {
+    super(message);
+
+    this.name = this.constructor.name;
+    // necessary?
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = {
+  ksqlDBError,
+  QueryBuilderError,
+  EmptyQueryError,
+  NumParamsError,
+  InappropriateStringParamError,
+  invalidArgumentTypes,
+};
---
+++ ksqljs/typescript/ksqldb/queryBuilder.ts
@@ -0,0 +1,86 @@
+const {
+  QueryBuilderError,
+  EmptyQueryError,
+  NumParamsError,
+  InappropriateStringParamError,
+} = require("./customErrors");
+import { IqueryBuilder } from "../types";
+
+class queryBuilder implements IqueryBuilder {
+  constructor() {}
+
+  build = (
+    query: string,
+    ...params: (string | number | boolean | string[])[]
+  ) => {
+    // check for empty query
+    if (this._checkEmptyQuery(query)) throw new EmptyQueryError();
+
+    let output = this._bind(query, ...params);
+
+    return output;
+  };
+  _bind = (
+    query: string,
+    ...params: (string | number | boolean | string[])[]
+  ) => {
+    const numParams = params.length;
+    // count num of ? in query
+    const numMark = query.split("?").length - 1;
+    if (numParams > numMark) {
+      throw new NumParamsError("more params than wildcards in query");
+    }
+    if (numParams < numMark) {
+      throw new NumParamsError("less params than wildcards in query");
+    }
+    for (let i = 0; i < numParams; i++) {
+      const newParam: string | number | boolean = this._replaceWith(params[i]);
+      // if (newParam instanceof Error) return newParam;
+      query = query.replace(/\?/, newParam as any);
+      // newParam above can be
+      // The `as any` syntax is called a type assertion and effectively
+      // turns off type checking for the specific parameter
+    }
+    return query;
+  };
+  _replaceWith = (param: string | number | boolean | string[]) => {
+    switch (typeof param) {
+      case "number":
+        return param;
+      case "bigint":
+        return param;
+      case "boolean":
+        return param;
+      case "object":
+        if (Array.isArray(param)) {
+          const el = param[0];
+          if (el?.includes(";")) {
+            throw new InappropriateStringParamError(
+              "string params not wrapped in quotes should not include semi-colons"
+            );
+          }
+          return `${el?.replaceAll("'", "''")}`;
+        }
+        throw new QueryBuilderError(
+          "object should not be passed in as query argument"
+        );
+      case "function":
+        throw new QueryBuilderError(
+          "function should not be passed in as query argument"
+        );
+      case "string":
+        // https://stackoverflow.com/questions/5139770/escape-character-in-sql-server
+        // example of injection in Go: https://go.dev/play/p/4KoWROjK903
+        return `'${param.replaceAll("'", "''")}'`;
+      default:
+      // code block
+    }
+  };
+  _checkEmptyQuery = (query: string) => {
+    if (query === "" || query === undefined || query === null) {
+      return true;
+    }
+    return false;
+  };
+}
+module.exports = queryBuilder;
